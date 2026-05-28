899aa0e38971d562e8f31ba08efee612e0d87676
PrathamRanka <pranka0789@gmail.com>
2026-05-28 18:36:58 +0530

    refactor: improve code readability with comments in main function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use anyhow::Result;
+use anyhow::Result; // type of import
 use iroh::{Endpoint, SecretKey};
 
-#[tokio::main]
+#[tokio::main] // attribute way of modifying code
 async fn main() -> Result<()> {
     let secret_key = SecretKey::generate(rand::rngs::OsRng);
     println!("> our secret key: {:?}", secret_key);
 
     let endpoint = Endpoint::builder()
+        .secret_key(secret_key)
         .discovery_n0()
         .bind()
         .await?;
-    println!("> our node id: {}", endpoint.node_id());
 
-    Ok(())
+    println!("> our node id: {}", endpoint.node_id());
+    Ok(()) // return Ok and ignore semicolon to make it the return value of the function
 }
