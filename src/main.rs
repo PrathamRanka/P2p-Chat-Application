18ecce7bf398a107bbc5dd6ca811ae9e57f5e149
PrathamRanka <pranka0789@gmail.com>
2026-05-28 18:32:12 +0530

    feat: initialize P2P chat project with basic functionality
    - Add Cargo.toml with dependencies for anyhow, iroh, rand, and tokio.
    - Create main.rs to generate a secret key and set up an endpoint.

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use anyhow::Result;
+use iroh::{Endpoint, SecretKey};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let secret_key = SecretKey::generate(rand::rngs::OsRng);
+    println!("> our secret key: {:?}", secret_key);
+
+    let endpoint = Endpoint::builder()
+        .discovery_n0()
+        .bind()
+        .await?;
+    println!("> our node id: {}", endpoint.node_id());
+
+    Ok(())
+}
